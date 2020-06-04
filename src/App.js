b07bd24c2cd4abf426615f25c3f92cda9bda1dc2
Melanie Locke <melanie.a.locke@gmail.com>
2020-06-03 20:49:22 -0600

    Add MainContent component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import NavBar from "./ui-components/NavBar.js";
+import MainContent from "./ui-components/MainContent.js";
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <MainContent />
     </div>
   );
 }
